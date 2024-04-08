a6a077d894336071dd19dda1c5a66188abe28d70
Ayush Mnayna <117539244+AyushManyan@users.noreply.github.com>
2024-04-08 18:03:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const fs=require('fs');
+const server=require('socket.io');
+
+
+const io = server(app.listen(3000));
+
+app.get('/', (req, res) => {
+    fs.readFile('index.html', 'utf8', function(err, data) {
+        if (err) throw err;
+        res.send(data);
+    });
+
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    let myname='';
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('user-disconnected', myname);
+    });
+    socket.on('chat message', (msg) => {
+        // console.log('message: ' + msg);
+        io.emit('chat message',msg);
+    }
+    );
+    socket.on('new-user', (msg) => {
+        myname=msg;
+        console.log('new user: ' + msg);
+        io.emit('new-user', msg);
+    });
+
+
+
+});
